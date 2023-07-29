829a324a1d53b6ee5f409b98daae11735f997060
vaibh36 <vaibh18@gmail.com>
2023-07-29 12:39:58 +0530

    chore: sitemap updates

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -2,64 +2,66 @@ export default function Sitemap() {
     return null;
 }
 
-async function generateSitemap() {
-    const siteMapArr = [
-        {
-            url: 'https://combatfit.in/',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/contact-us',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/vision',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/clothing-category?item=tactical',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/clothing-category?item=winter',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/items/coffee-mugs',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/items/beer',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/items/posters',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://combatfit.in/items/whiskey',
-            lastModified: new Date(),
-        },
-    ];
-
+async function generateSiteMap(posts) {
+    const EXTERNAL_DATA_URL = 'https://combatfit.in/';
     return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     
-        ${siteMapArr
-            ?.map((page) => {
-                return `<url>
-
-            <loc>${page.url}</loc>
-                <lastmod>${page.lastModified}</lastmod>
-            </url>`;
-            })
-            .join('')}
-      </urlset>`;
+     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <!--We manually set the two URLs we know already-->
+       <url>
+         <loc>https://combatfit.in/</loc>
+       </url>
+      
+       ${posts
+           .map(({ id }) => {
+               return `
+         <url>
+             <loc>${`${EXTERNAL_DATA_URL}${id}`}</loc>
+         </url>
+       `;
+           })
+           .join('')}
+     </urlset>
+   `;
 }
 
 export const getServerSideProps = async (ctx) => {
     ctx.res.setHeader('Content-Type', 'application/xml');
-    const xml = await generateSitemap();
+    // const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+    // const request = await fetch(EXTERNAL_DATA_URL);
+    const posts = [
+        {
+            id: 'vision',
+        },
+        {
+            id: 'clothing-category?item=active',
+        },
+        {
+            id: 'clothing-category?item=tactical',
+        },
+        {
+            id: 'clothing-category?item=inspire',
+        },
+        {
+            id: 'clothing-category?item=winter',
+        },
+        {
+            id: 'contact-us',
+        },
+        {
+            id: 'items/coffee-mugs',
+        },
+        {
+            id: 'items/beer',
+        },
+        {
+            id: 'items/posters',
+        },
+        {
+            id: 'items/whiskey',
+        },
+    ];
+
+    const xml = await generateSiteMap(posts);
 
     ctx.res.write(xml);
     ctx.res.end();
